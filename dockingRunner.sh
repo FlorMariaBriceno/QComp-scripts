6e6b39f4076f0d232f9ffb9cdcb9347c69313fc4
FlorMariaBriceno <florecitabriceno@hotmail.com>
2025-11-19 19:51:54 -0600

    Corrección de errores

--- dockingRunner.sh
+++ dockingRunner.sh
@@ -13,16 +13,16 @@ la variante de este script adaptada a los archivos .pdbqt de Autodock Vina
 
 El script se ejecuta como:
 
-./dockingRunner.sh > salida.out &
+./dockingRunner.sh 
 
-Posiblemente sea necesario concederle el permiso de ejecución con
+Posiblemente sea necesario concederle el permiso de ejecución previamente con
 
 chmod +x dockingRunner.sh
 
-Esto dirige toda la información del estatus de los cálculos a un archivo de salida
-y a ejecución en segundo plano, para que el terminal continúe utilizable. ' 
+Cualquier duda conmigo, por favor' 
 
 #!/bin/bash
+# Parte 1. Introducir el nombre del ligando
 read -p "Introduce el nombre del archivo .dpf del ligando: " dpf_file
 if [ -z ${dpf_file} ]; # prueba para evitar dejar la variable vacía
 then 
@@ -32,14 +32,28 @@ else
     echo -e "El nombre de los archivos de salida será i-${dpf_file}.dlg.pdb"
 fi
 
-for i in {1..10..1} # inicializacion de un ciclo for para la variable i, {1..# de repeticiones..intervalo}
-do    
+# Parte 2.  Introducir el numero de repeticiones
+read -p "Introduce el número de repeticiones para el docking: " rep
+if [ -z ${rep} ]; # prueba para evitar dejar la variable vacía
+then 
+    echo "No se introdujo el nombre del archivo"
+    exit 1 # salir del programa reportando que hubo un problema
+else
+    echo -e "Se harán ${rep} repeticiones del docking con el ligando: ${dpf_file}.dpf"
+fi
+
+# Parte 3. Ejecución cíclica
+for i in $(seq 1 $rep) # secuencia desde 1 hasta el número de repeticiones
+do
     ini=$(date + '%C') # fecha y hora de inicio de la corrida i
-    echo -e "Hora de inicio de corrida $i: $ini"
+    echo -e "Hora de inicio de corrida $i: $ini" | tee $i-$dpf_file.pdb.dlg
     autodock4 -p $dpf_file -l $i-$dpf_file.dlg.pdb & # ejecución del docking número i
-    echo -e "PID del docking $i: $!" # ID de programa (PID) de cada corrida
+    echo -e "PID del docking $i: $!" | tee -a $i-$dpf_file.pdb.dlg # ID de programa (PID) de cada corrida
     wait $! # esperar a que el cálculo acabe, esto hace que se ejecuten de manera consecutiva sin saturar la máquina
-    fin=$(date + '%C') # fecha y hora de finalización del docking numero i
-    echo -e "Hora de finalización de corrida $i: $fin\n"
-    # echo -e "$i-$dpf_file\n" # Esto era para checar que el ciclo funcione bien
+    fin=$(date + '%C') | tee $i-$dpf_file.pdb.dlg # fecha y hora de finalización del docking numero i
+    echo -e "Hora de finalización de corrida $i: $fin\n" | tee -a $i-$dpf_file.pdb.dlg
+    #echo -e "$i-$dpf_file\n" | tee $i-$dpf_file.dlg.pdb
+    #sleep 15
+    #rm $i-$dpf_file.dlg.pdb  # Esto era para checar que el ciclo funcione bien
 done
+exit 0
